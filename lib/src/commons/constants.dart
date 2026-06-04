59f17ffaefb656a2ed6ee585b85f25f91bc8b63d
hungkv <kimvnhung@gmail.com>
2026-06-04 11:46:12 +0700

    fix: correct dimensions for desktopMinimumSize and landscapeDesktopMinimumSize constants

--- lib/src/commons/constants.dart
+++ lib/src/commons/constants.dart
@@ -1,12 +1,12 @@
 import 'dart:ui';
 
 class Constants {
-  static const Size desktopMinimumSize = Size(1366, 860);
+  static const Size desktopMinimumSize = Size(860, 1366);
   static const Size tabletSize = Size(1024, 1366);
   static const Size smallTabletSize = Size(768, 1024);
   static const Size mobileSize = Size(430, 932);
 
-  static const Size landscapeDesktopMinimumSize = Size(860, 1366);
+  static const Size landscapeDesktopMinimumSize = Size(1366, 860);
   static const Size landscapeTabletSize = Size(1366, 1024);
   static const Size landscapeSmallTabletSize = Size(1024, 768);
   static const Size landscapeMobileSize = Size(932, 430);
